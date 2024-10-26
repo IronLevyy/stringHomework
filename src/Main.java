b8e0202ebf85b8f02c53db058247190a89152a23
Iron Levyy <olegan.plav@gmail.com>
2024-10-27 00:10:16 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.printf("ФИО сотрудника - %s", fullName);
+        System.out.print("\n");
+
+//        Задача 2
+        String fullNameUp = fullName.toUpperCase();
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета — %s\n", fullNameUp);
+
+//        Задача 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.printf("Данные ФИО сотрудника — %s", fullName);
     }
 }
